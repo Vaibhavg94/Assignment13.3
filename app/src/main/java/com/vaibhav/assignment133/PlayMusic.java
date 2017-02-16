1622a8a74e1af8df22aff6c2a1262c8426057292
Vaibhav Gupta <guptvaibhav.gupta@gmail.com>
2017-02-16 22:57:20 +0530

    Initial commit

---
+++ app/src/main/java/com/vaibhav/assignment133/PlayMusic.java
@@ -0,0 +1,36 @@
+package com.vaibhav.assignment133;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class PlayMusic extends Service {
+
+    MediaPlayer mp;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mp = MediaPlayer.create(this, R.raw.audio);
+        mp.start();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mp.stop();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
